243ce2371826b43d3bae891f5addaffa81c3d0d1
Vorrawut Judasri <odd-vorrawut.judasri@tech.jago.com>
2024-10-07 00:11:32 +0700

    Add more contact point

--- src/components/home/sidenav/Sidenav.js
+++ src/components/home/sidenav/Sidenav.js
@@ -6,33 +6,28 @@ const Sidenav = () => {
   const { getNormalLightText } = useTheme()
   return (
     <div className='px-7 py-4'>
-      {/*<SidenavTitle title='M' subTitle='enu' />*/}
-      {/*<ul>*/}
-      {/*  <li className='sidenavLi'>Blog Page</li>*/}
-      {/*  <li className='sidenavLi'>Portfolio Page</li>*/}
-      {/*</ul>*/}
-      {/*<SidenavTitle title='P' subTitle='rojects' />*/}
-      {/*<ul>*/}
-      {/*  <li className='sidenavLi'>Web Development</li>*/}
-      {/*  <li className='sidenavLi'>E-commerce</li>*/}
-      {/*  <li className='sidenavLi'>Chatting Applications</li>*/}
-      {/*  <li className='sidenavLi'>Portfolio Websites</li>*/}
-      {/*  <li className='sidenavLi'>Backend Setup</li>*/}
-      {/*  <li className='sidenavLi'>Amazon Clone</li>*/}
-      {/*</ul>*/}
-      {/*<SidenavTitle title='L' subTitle='atest Posts' />*/}
-      {/*<ul>*/}
-      {/*  <li className='sidenavLi'>UI & UX Conference at Lviv 2022</li>*/}
-      {/*  <li className='sidenavLi'>How to become a creative designer</li>*/}
-      {/*  <li className='sidenavLi'>Designers thoughts about mobile UI templates</li>*/}
-      {/*  <li className='sidenavLi'>Designer Conference at Florida, USA 2020</li>*/}
-      {/*</ul>*/}
       <SidenavTitle title='R' subTitle='each Me' />
       <ul>
         <li className={`sidenavLi ${getNormalLightText}`}>+66 922683583</li>
-        <li className={`sidenavLi ${getNormalLightText}`}>vorrawutjud.work@gmail.com</li>
+        <li className={`sidenavLi ${getNormalLightText}`}>
+          <a href='mailto:vorrawutjud.work@gmail.com'>vorrawutjud.work@gmail.com</a>
+        </li>
+        <li className={`sidenavLi ${getNormalLightText}`}>
+          <a href='https://github.com/vorrawut' target='_blank' rel='noopener noreferrer'>
+            Github
+          </a>
+        </li>
+        <li className={`sidenavLi ${getNormalLightText}`}>
+          <a href='https://medium.com/@vortj' target='_blank' rel='noopener noreferrer'>
+            Medium
+          </a>
+        </li>
+        <li className={`sidenavLi ${getNormalLightText}`}>
+          <a href='https://www.linkedin.com/in/vorrawut-judasri-b10663ba' target='_blank' rel='noopener noreferrer'>
+            Linkedin
+          </a>
+        </li>
       </ul>
-      {/*<MusicPlayer />*/}
     </div>
   )
 }
