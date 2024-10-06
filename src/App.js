6e1bf2ac126fbf0dbd56f7a3a80deb08175e6f33
Vorrawut Judasri <odd-vorrawut.judasri@tech.jago.com>
2024-10-06 16:50:55 +0700

    Add google Analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import RoundOne from './components/roundDesigns/RoundOne'
 import RoundTwo from './components/roundDesigns/RoundTwo'
 import RoundThree from './components/roundDesigns/RoundThree'
 import RoundFour from './components/roundDesigns/RoundFour'
 import RoundFive from './components/roundDesigns/RoundFive'
 import Home from './Home'
+import { initialize, pageview } from 'react-ga'
 
 function App() {
+  const TRACKING_ID = 'G-RX522EVDM5' // Replace with your Google Analytics tracking ID
+  initialize(TRACKING_ID)
+
+  useEffect(() => {
+    pageview(window.location.pathname + window.location.search)
+  }, [])
+
   return (
     <div className='w-full lgl:h-screen font-bodyfont overflow-hidden text-textColor bg-slate-900 relative'>
       <div className='max-w-screen-2xl h-full mx-auto flex justify-center items-center'>
