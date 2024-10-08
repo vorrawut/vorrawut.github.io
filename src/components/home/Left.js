2ddde609452b89676c2cc6e8f3943f6a8c14451f
Vorrawut Judasri <odd-vorrawut.judasri@tech.jago.com>
2024-10-08 09:25:13 +0700

    Adjust the padding Left

--- src/components/home/Left.js
+++ src/components/home/Left.js
@@ -30,7 +30,7 @@ const Left = () => {
         <img className='w-full h-full object-cover rounded-2xl' src={bannerImage} loading='priority' alt='bannerImage' />
       </div>
       <div className='w-full h-2/5 flex flex-col justify-between border-t-0 rounded-bl-xl rounded-br-xl'>
-        <div className='flex flex-col items-center gap-2 py-10'>
+        <div className='flex flex-col items-center gap-2 py-8'>
           <h1 className={`${getBGWithNormalText} text-4xl font-semibold`}>Vorrawut Judasri</h1>
           <p className={`${getBGWithColorText} text-base tracking-wide`}>
             {text}
